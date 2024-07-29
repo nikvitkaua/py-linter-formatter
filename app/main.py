64ff9f1af972fc2701342eff41a03e294b1d786d
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-07-29 21:18:48 +0300

    add solution for task #1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
 def format_linter_error(error: dict) -> dict:
-    # write your code here
-    pass
+    return {
+        "line": error.get("line_number"),
+        "column": error.get("column_number"),
+        "message": error.get("text"),
+        "name": error.get("code"),
+        "source": "flake8"
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
