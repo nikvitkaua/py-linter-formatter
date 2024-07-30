082e8791eed50b2fc76622bf02193eaa077fe436
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-07-30 09:36:12 +0300

    change variable name

--- app/main.py
+++ app/main.py
@@ -13,13 +13,13 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
         "errors":
             [
                 {
-                    "line": err.get("line_number"),
-                    "column": err.get("column_number"),
-                    "message": err.get("text"),
-                    "name": err.get("code"),
+                    "line": error.get("line_number"),
+                    "column": error.get("column_number"),
+                    "message": error.get("text"),
+                    "name": error.get("code"),
                     "source": "flake8"
                 }
-                for err in errors
+                for error in errors
             ],
         "path": file_path,
         "status": "failed"
